6c2bd23ecab68913e52a3c9ee89d061ff67867bb
allison405 <allisonlee422@gmail.com>
2025-10-29 19:13:21 -0400

    Background, search bar, menu

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
 // Background script for the browser extension will go here
+
+
+const sidebar = document.getElementById('sidebar');
+const menubtn = document.getElementById('menubtn');
+const closemenu = document.getElementById('closemenu');
+
+menubtn.addEventListener('click', () => {
+    sidebar.style.left = '0';
+});
+
+closemenu.addEventListener('click', () => {
+    sidebar.style.left = '-250px';
+});
