82e878e4573378e32bf279a0543e201bc4ba6552
allison405 <allisonlee422@gmail.com>
2025-11-26 20:15:50 -0500

    new pages

--- background.js
+++ background.js
@@ -5,6 +5,9 @@ const menu = document.getElementById('menu');
 const openmenu = document.getElementById('openmenu');
 const closemenu = document.getElementById('closemenu');
 const addRecipe = document.getElementById('addRecipe');
+const savedRecipes = document.getElementById('savedRecipes');
+const groceryList = document.getElementById('groceryList');
+const calendar = document.getElementById('calendar');
 
 openmenu.addEventListener('click', () => {
     menu.style.left = '0';
@@ -16,4 +19,16 @@ closemenu.addEventListener('click', () => {
 
 addRecipe.addEventListener('click', () => {
     window.location.href = 'addRecipe.html';
+});
+
+savedRecipes.addEventListener('click', () => {
+    window.location.href = 'savedRecipes.html';
+});
+
+groceryList.addEventListener('click', () => {
+    window.location.href = 'groceryList.html';
+});
+
+calendar.addEventListener('click', () => {
+    window.location.href = 'calendar.html';
 });
